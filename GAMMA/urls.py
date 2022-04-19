a372ecca9eeab7307b079117c16dbe61cd31ceca
moosaa95 <moosaabdullahi45@gmail.com>
2022-04-19 15:09:17 +0000

    add blog app urls to main site urls

--- GAMMA/urls.py
+++ GAMMA/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('course/', include('courses.urls')),
     path('auth/', include('users.urls')),
+    path('blog/', include('blog.urls')),
 ]
 
 # add static
